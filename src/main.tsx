792fcf2a9ed6742622463a5086eeb1b35637ff32
EdiedRamos <edieisa22@gmail.com>
2024-04-28 01:38:44 -0500

    chore: add font and colors

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import "./general/styles/index.scss";
+import "./general/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
