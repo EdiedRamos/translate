057d35b0a5bba6093aab5689ce62eabd3a1b815f
EdiedRamos <edieisa22@gmail.com>
2024-04-28 01:14:01 -0500

    chore: add path dependency and configure absolute route alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// @ts-expect-error no types provided
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
+});
