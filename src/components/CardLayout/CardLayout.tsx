c60494725d030760492a91aaf711d0f301ac9090
EdiedRamos <edieisa22@gmail.com>
2024-04-28 19:49:19 -0500

    chore: add className prop in CardLayout

--- src/components/CardLayout/CardLayout.tsx
+++ src/components/CardLayout/CardLayout.tsx
@@ -4,8 +4,9 @@ import "./CardLayout.scss";
 
 type CardLayoutProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export const CardLayout = ({ children }: CardLayoutProps) => {
-  return <div className="card-layout">{children}</div>;
+export const CardLayout = ({ children, className = "" }: CardLayoutProps) => {
+  return <div className={`card-layout ${className}`}>{children}</div>;
 };
