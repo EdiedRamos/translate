ba2629e500aa8be467f154f640947701a6052570
EdiedRamos <edieisa22@gmail.com>
2024-08-01 03:51:04 -0500

    feat: add language selected logic

--- src/components/Languages/Languages.tsx
+++ src/components/Languages/Languages.tsx
@@ -1,15 +1,22 @@
 import "./Languages.scss";
 
 import { LanguageButton } from "../LanguageButton/LanguageButton";
+import { useState } from "react";
+
+const DETECT_LANGUAGE = {
+  id: "6bb226c9-f565-4a75-b332-d6f1973b27d7",
+  value: "Detect Language",
+};
 
 const INITIAL_OPTIONS = [
-  { id: "1", value: "English" },
-  { id: "2", value: "French" },
+  { id: "8a9b9cfc-860a-484c-aa79-583071500494", value: "English" },
+  { id: "9003d0b9-b9e8-483e-aae7-5839188c17ff", value: "English" },
+  { id: "0981618e-eac0-4f00-ba5a-c0a5896e3f93", value: "French" },
 ];
 const SELECT_OPTIONS = [
-  { id: "3", value: "Spanish" },
-  { id: "4", value: "Italian" },
-  { id: "5", value: "Portuguese" },
+  { id: "396b2ed9-5f69-4deb-8751-68bc081218b7", value: "Spanish" },
+  { id: "3924b033-e3db-4c67-8fbd-e8e7a1a16d0c", value: "Italian" },
+  { id: "71ff27df-433b-4a7b-9903-54dff6107653", value: "Portuguese" },
 ];
 
 interface Props {
@@ -17,22 +24,50 @@ interface Props {
 }
 
 export const Languages = ({ withDetectionLanguage = false }: Props) => {
+  const [selected, setSelected] = useState<string>(
+    withDetectionLanguage ? DETECT_LANGUAGE.id : INITIAL_OPTIONS[0]?.id
+  );
+
   return (
     <div className="languages languages-container">
       {withDetectionLanguage && (
-        <LanguageButton>Detect Language</LanguageButton>
+        <LanguageButton
+          onClick={() => setSelected(DETECT_LANGUAGE.id)}
+          className={
+            DETECT_LANGUAGE.id === selected ? "language-button--active" : ""
+          }
+        >
+          {DETECT_LANGUAGE.value}
+        </LanguageButton>
       )}
       {INITIAL_OPTIONS.map(({ id, value }) => (
         <LanguageButton
-          // className={ === 1 ? "language-button--active" : ""}
+          onClick={() => setSelected(id)}
+          className={id === selected ? "language-button--active" : ""}
           key={id}
         >
           {value}
         </LanguageButton>
       ))}
-      <select>
+      <select
+        className={
+          SELECT_OPTIONS.some((option) => option.id === selected)
+            ? "language-button--active"
+            : ""
+        }
+        onChange={(event) => setSelected(event.target.value)}
+      >
+        <option
+          value={
+            withDetectionLanguage ? DETECT_LANGUAGE.id : INITIAL_OPTIONS[0]?.id
+          }
+        >
+          Other language
+        </option>
         {SELECT_OPTIONS.map(({ id, value }) => (
-          <option key={id}>{value}</option>
+          <option selected={selected === id} key={id} value={id}>
+            {value}
+          </option>
         ))}
       </select>
     </div>
