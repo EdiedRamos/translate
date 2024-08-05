de2913bbe74e5972f4be5fffd93074bfcfbcfe43
EdiedRamos <edieisa22@gmail.com>
2024-08-05 11:05:39 -0500

    refactor: remove comments

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -9,17 +9,12 @@ interface Props {
 }
 
 export const Text = ({ isDisable = false, value, onChangeValue }: Props) => {
-  // const [text, setText] = useState<string>(
-  //   isDisable ? "Bonjour, comment allez-vous?" : "Hello, how are you?"
-  // );
-
   return (
     <div className="text text__container">
       <textarea
         className="text__text-area"
         disabled={isDisable}
         value={value}
-        // onChange={(event) => setText(event.target.value.substring(0, 500))}
         onChange={onChangeValue}
       />
       {!isDisable && <p className="text__length">{value.length}/500</p>}
