e54e4ecc432ea7e3fd38caa9a954b217ba47c95f
EdiedRamos <edieisa22@gmail.com>
2024-08-05 11:11:03 -0500

    feat: add react-toastify container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
 import { Translate } from "./pages";
 
 const App = () => {
-  return <Translate />;
+  return (
+    <>
+      <ToastContainer />
+      <Translate />
+    </>
+  );
 };
 
 export default App;
